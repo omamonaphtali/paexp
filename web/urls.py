efdf1a86c5a81dfc82db712ae611b124cce84c14
omamonaphtali <omamonaphtali@gmail.com>
2018-06-11 13:05:42 +0300

    fixed routing bug

--- web/urls.py
+++ web/urls.py
@@ -9,11 +9,12 @@ app_name = 'web'
 
 urlpatterns = [
     url(r'^$', views.index_view, name='home'),
+    url(r'^home/', views.index_view, name='home'),
     url(r'^profile/', views.about_view, name='profile'),
     url(r'^services/', views.services_view, name='services'),
     url(r'^rental/', views.rental_view, name='rental'),
     url(r'^gallery/', views.gallery_view, name='gallery'),
-    url(r'^contact/', views.contact_view, name='contacts'),
+    url(r'^contacts/', views.contact_view, name='contacts'),
     url(r'^blog/', views.blog_view, name='blog'),
     url(r'^topography/', views.topography_view, name='topography'),
     url(r'^projects/', views.projects_view, name='projects'),
